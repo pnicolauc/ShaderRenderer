af6f24bc83cbb954d5c821dbf54ff8635d098ae8
Pedro Carvalho <80.nicolau@gmail.com>
2018-01-19 18:01:22 +0000

    time and res variables added

--- script.js
+++ script.js
@@ -10,36 +10,38 @@ const VIEW_ANGLE = 45,
     ASPECT =1,
     NEAR = 0.1,
     FAR = 10000;
+
+var renderer, scene;
 var camera = new THREE.Camera(  VIEW_ANGLE,
                                 ASPECT,
                                 NEAR,
                                 FAR  );
-run();
+init();
+var startTime = Date.now();
+animate();
 
-function run() {
+function init() {
+
+  uniforms = {
+  			time: { type: "f", value: 1.0 },
+  			resolution: { type: "v2", value: new THREE.Vector2() }
+  };
   var container = document.getElementById("renderer");
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
-  var renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer();
   container.appendChild(renderer.domElement);
   //renderer = new THREE.WebGLRenderer();
-
-
-
-	var scene = new THREE.Scene();
-
+	scene = new THREE.Scene();
 	// the camera starts at 0,0,0 so pull it back
 	camera.position.z = 300;
 	// create the sphere's material
 	var shaderMaterial = new THREE.MeshShaderMaterial({
+    uniforms:       uniforms,
 		vertexShader:   vertexshader,
 		fragmentShader: document.getElementById("fragmentshader").value
 	});
-
-	// set up the sphere vars
-	var radius = 50, segments = 16, rings = 16;
-
 	// create a new mesh with sphere geometry -
 	// we will cover the sphereMaterial next!
 	var plane = new THREE.Mesh(
@@ -49,7 +51,21 @@ function run() {
 	// add the sphere to the scene
 	scene.addChild(plane);
 
+  uniforms.resolution.value.x = 400;
+	uniforms.resolution.value.y = 400;
+
 	// draw!
-	renderer.render(scene, camera);
+	//renderer.render(scene, camera);
 
   };
+
+  function animate() {
+		requestAnimationFrame( animate );
+		render();
+	}
+	function render() {
+		var elapsedMilliseconds = Date.now() - startTime;
+		var elapsedSeconds = elapsedMilliseconds / 1000.;
+		uniforms.time.value = 60. * elapsedSeconds;
+		renderer.render( scene, camera );
+	}
